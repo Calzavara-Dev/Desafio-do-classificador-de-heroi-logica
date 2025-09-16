7da3f964a14ee34798fecdbc116c9c60565b69ab
Calzavara-Dev <erickcalzav@gmail.com>
2025-09-16 19:22:47 -0300

    reatuzalizando

---
+++ classificador-de-heroi-javascript/src/index.js
@@ -0,0 +1,24 @@
+// Classificador De Nível do herói!!
+let heroi = "Razziel"
+let xp = 5500
+    if(xp <=1000){
+        
+            console.log("O Herói de nome "+heroi,"está no nível de Ferro") 
+        }
+        else if(xp >= 1001 && xp <= 2000){
+            console.log("O Herói de nome "+heroi,"está no nível de Bronze") 
+        }
+         else if(xp >= 2001 && xp <= 5000){
+            console.log("O Herói de nome "+heroi,"está no nível de Prata") 
+        }
+         else if(xp >= 5001 && xp <= 6000){
+            console.log("O Herói de nome "+heroi,"está no nível de Ouro") 
+        }
+         else if(xp >= 6001 && xp <= 7000){
+            console.log("O Herói de nome "+heroi,"está no nível de Imortal") 
+        }
+        else{
+            console.log("O Herói de nome "+heroi,"está no nível de Radiante")
+
+        }
+    
